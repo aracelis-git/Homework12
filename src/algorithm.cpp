8f8a7f9d8750556afea9ec30915f6162b87944cb
viki <afenixiwolfr@hotmail.com>
2017-04-18 15:08:23 -0500

    Created publisher and subscriber functions in algorithm class

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -1,4 +1,4 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
 #include "std_msgs/String.h"
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
@@ -7,40 +7,73 @@
 
 class Algorithm
 {
+
 public:
   Algorithm(const ros::NodeHandle& n);
+  void scanCallback(const sensor_msgs::LaserScan::ConstPtr& dist);
+  void runAlgo();
 
 private:
   ros::NodeHandle n_;
   ros::Subscriber scan_sub_;
   ros::Publisher twist_pub_;
+  double linear_, angular_, lscale_, ascale_, range;
 
 };
 
 Algorithm::Algorithm(const ros::NodeHandle& n)
-	: n_(n)
+	: n_(n),
+	  linear_(0),
+	  angular_(0),
+	  lscale_(1.0),
+	  ascale_(1.0)
 {
-  scan_sub_ = n_.subscribe("scan", 1000, scanCallback);
+  scan_sub_ = n_.subscribe("scan", 1000, &Algorithm::scanCallback, this);
   twist_pub_ = n_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
 }
 
-void scanCallback(const geometry_msgs::Twist::ConstPtr& dist)
+void Algorithm::scanCallback(const sensor_msgs::LaserScan::ConstPtr& dist)
 {
-  last_command_time_ = ros::WallTime::now();
-  lin_vel_ = vel->linear.x;
-  ang_vel_ = vel->angular.z;
+  range = dist->ranges[320];
+}
+
+void Algorithm::runAlgo()
+{
+  ros::spinOnce();
+  if (range != range || range >= 0.5)
+  {
+    linear_ = 2.0;
+  }
+  else if (range < 0.5)
+  {
+    angular_ = 2.0;
+  }
+  else
+  {
+	linear_ = 0.0;
+	angular_ = 0.0;
+  }
+
+  geometry_msgs::Twist twist;
+  twist.angular.z = ascale_*angular_;
+  twist.linear.x = lscale_*linear_;
+  twist_pub_.publish(twist);
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "teleop_turtle");
-  Algorithm walker_algorithm;
-
-
+  ros::init(argc, argv, "walker_algorithm");
+  Algorithm walker_algorithm(ros::NodeHandle(""));
+  while (ros::ok())
+  {
+    walker_algorithm.runAlgo();
+  }
 
   return(0);
 
 }
+
+
 
 
 
